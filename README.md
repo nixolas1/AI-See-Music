c05cd71a9963f45ff5c842d7f55949ee0679e938
Nicolas <nicolas.tonne@makingwaves.com>
2018-11-16 11:39:00 +0100

    Fix

---
+++ README.md
@@ -0,0 +1,23 @@
+# Typescript Three.js Webpack Starter
+
+- Typescript support.
+- Webpack
+- Html plugin
+- BrowserSync
+
+## Usage
+
+### Install Typscript
+
+```
+npm install typescript -g
+```
+
+### Start
+
+```
+$ git clone https://github.com/pinqy520/three-typescript-starter.git
+$ cd three-typescript-starter
+$ npm install # or yarn
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+/* Configure HTMLWebpack plugin */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+/* Export configuration */
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: [
+        './src/index.ts'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'awesome-typescript-loader'
+            }, {
+                test: /\.css$/,
+                exclude: /[\/\\]src[\/\\]/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {loader: 'css-loader'}
+                ]
+            }, {
+                test: /\.css$/,
+                exclude: /[\/\\](node_modules|bower_components|public)[\/\\]/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: { extensions: [".web.ts", ".web.js", ".ts", ".js"] },
+    plugins: [HTMLWebpackPluginConfig]
+}
